34763e20ac49680e5d0c41d5e17bc006c6b4af2b
jorge.henao <jorge.henao@ceiba.com.co>
2021-05-20 06:19:51 -0500

    Ajustes testing consultar compania

--- microservicio/dominio/src/main/java/com/ceiba/nomina/servicio/ServicioCrearNomina.java
+++ microservicio/dominio/src/main/java/com/ceiba/nomina/servicio/ServicioCrearNomina.java
@@ -1,5 +1,6 @@
 package com.ceiba.nomina.servicio;
 
+import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 import com.ceiba.nomina.modelo.entidad.Nomina;
 import com.ceiba.nomina.puerto.repositorio.RepositorioNomina;
 import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
@@ -33,7 +34,7 @@ public class ServicioCrearNomina {
 
      private void validarFechaCreacion(Nomina nomina) {
         if (nomina.getFechaCreacion().getDayOfMonth() > 14) {
-            throw new RuntimeException("La fecha de registro no puede ser superior al dia 14");
+            throw new ExcepcionValorInvalido("La fecha de registro no puede ser superior al dia 14");
         }
     }
 }
